67fe9fbf364b33f1ff32a75b0ffd806ea427d22b
Mark Tamarov <googlcraftlol@gmail.com>
2020-05-26 23:58:11 +0300

    refactoring

---
+++ apiWorker.js
@@ -0,0 +1,60 @@
+class ApiWorker {
+
+    constructor(link, type='') {
+      this.link = link;
+      this.type = type.toUpperCase();
+    }
+
+    async simpleExecute(){
+        try {
+            
+            const response = await fetch(this.link)
+            const data = await response.json()
+            return data
+
+        } catch (err) {
+            return err
+        }
+    }
+
+    async execute(body){
+        try {
+
+            async function executeWithBody(met){
+
+                const requestOptions = {
+                    method: met,
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(body)
+                  };
+                
+                const response = await fetch(this.link, requestOptions);
+                const data = await response.json()
+                return data
+            }
+
+            switch (this.type) {
+                case 'POST':
+                    executeWithBody(this.type);
+                    break;
+                case 'PUT':
+                    executeWithBody(this.type);
+                    break;
+                case 'PATCH':
+                    executeWithBody(this.type);
+                    break;
+                default:
+                    throw 'method type error';
+              }
+            
+
+        } catch (err) {
+            return err
+        }
+    }
+
+  }
+
+module.exports = {
+    ApiWorker: ApiWorker
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+//const ApiWorker = require('./apiWorker.js')
